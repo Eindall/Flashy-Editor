d62d4a285c0b24a5824d2a55ed610131341978d2
Thomas Marquis <tmarquis@maskott.com>
2020-03-04 23:26:27 +0100

    Fixed some warnings in code

--- Nouveau Projet De Jeu/Player/Player.gd
+++ Nouveau Projet De Jeu/Player/Player.gd
@@ -11,13 +11,14 @@ var y_velocity = 0
 func _ready():
 	pass # Replace with function body.
 
-func _process(delta):
+func _process(_delta):
 	var move_dir = 0
 	if Input.is_action_pressed("move_right"):
 		move_dir += 1
 	if Input.is_action_pressed("move_left"):
 		move_dir -= 1
 		
+	# warning-ignore:return_value_discarded
 	move_and_slide(Vector2(move_dir * MOVE_SPEED, y_velocity), Vector2(0, -1))
 	
 	var grounded = is_on_floor()
